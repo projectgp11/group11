4e529a879533c8a71665554297edd7b4d2b0e1a8
projectgp11 <36749476+projectgp11@users.noreply.github.com>
2018-04-02 20:46:44 +0100

    Add files via upload
    provisional (work in progress)

---
+++ screens/MyCamera.js
@@ -0,0 +1,70 @@
+import React, { PureComponent } from 'react'
+import { AppRegistry, StyleSheet, Text, View, Dimensions, Button } from 'react-native'
+import { NavigationComponent } from 'react-native-material-bottom-navigation'
+import { TabNavigator } from 'react-navigation'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+import {Camera} from 'react-native-camera';
+
+
+export default class Home extends PureComponent<void, *, void> {
+  static navigationOptions = {
+	tabBarLabel: '',
+    tabBarIcon: () => <Icon size={25} name="home" color="white"/>
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar
+          hidden
+        />
+        <Camera
+          ref={(cam) => {
+            this.props.setCam(cam);
+          }}
+          captureQuality={Camera.constants.CaptureQuality['480p']}
+          captureTarget={Camera.constants.CaptureTarget.memory}
+          playSoundOnCapture={false}
+          jpegQuality={40}
+          style={styles.preview}
+          aspect={Camera.constants.Aspect.fill}
+        />
+        {this.props.children}
+      </View>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+
+container: {  
+flex: 1,   
+backgroundColor: '#00ffffff',
+justifyContent: 'center',
+},
+preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width,
+  },
+para: {
+	fontSize: 21,
+	color: '#1c2a43',
+	fontWeight:'500',
+	marginTop:10,
+	alignSelf: 'center',
+},
+MyNav: {
+          height: 160,
+          elevation: 8,
+          position: 'absolute',
+		  left: 0,
+		  right: 0,
+          bottom: 0,
+   },
+});
+
+
+AppRegistry.registerComponent('Home', () =>Home);
